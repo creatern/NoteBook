3d113f0355bd494f8ada679d4cbd341afcd89a42
zjk <zjk1054860443@163.com>
2024-03-17 15:00:40 +0800

    set-up

--- ishell/setup.sh
+++ ishell/setup.sh
@@ -1,27 +1,5 @@
 #!/bin/bash"
 
-# if did not clone
-# cd ~
-# git clone https://gitee.com/zjk1054860443/note-book.git
-
-cd ~/note-book/ishell
-ISHELL_POSITION=$(pwd)
-cd $ISHELL_POSITION;
-cd ..;
-BOOK_HOME=$(pwd)
-cd $BOOK_HOME/Projects
-I_PROJECTS=$(pwd)
-
-# set env
-echo 'export I_PROJECTS=$BOOK_HOME/Projects' >> ~/.bashrc
-echo 'alias ok="$BOOK_HOME/ishell/igit.sh 0"' >> ~/.bashrc
-echo 'alias lock="$BOOK_HOME/ishell/igit.sh 1"' >> ~/.bashrc
-echo 'alias end="$BOOK_HOME/ishell/igit.sh 2"' >> ~/.bashrc
-
-echo "=====env ok"
-
-# set git
-cd $BOOK_HOME
 if git --version
 then
 	echo "git ok"
@@ -43,9 +21,38 @@ fi
 
 echo "=====git ok"
 
+if (cd ~/note-book)
+then
+	echo "=====git clone ok"
+else
+	cd ~
+	git clone https://gitee.com/zjk1054860443/note-book.git
+fi
+
+cd ~/note-book/ishell
+ISHELL_POSITION=$(pwd)
+cd $ISHELL_POSITION;
+cd ..;
+BOOK_HOME=$(pwd)
+cd $BOOK_HOME/Projects
+I_PROJECTS=$(pwd)
+
+# set env
+echo 'export I_PROJECTS=$BOOK_HOME/Projects' >> ~/.bashrc
+echo 'alias ok="$BOOK_HOME/ishell/igit.sh 0"' >> ~/.bashrc
+echo 'alias lock="$BOOK_HOME/ishell/igit.sh 1"' >> ~/.bashrc
+echo 'alias end="$BOOK_HOME/ishell/igit.sh 2"' >> ~/.bashrc
+
+echo "=====env ok"
+
+# set git
+cd $BOOK_HOME
+
 git remote add gitee git@gitee.com:zjk1054860443/note-book.git
 git remote add github github git@github.com:creatern/NoteBook.git
 git remote remove origin
+
+echo "=====git remote ok"
 
 # set ssh: rsa.pub
 cd ~;
