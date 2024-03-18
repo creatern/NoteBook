7ddeab8efbd87e33b55b404ca174a5e37baec989
zjk <zjk1054860443@163.com>
2024-03-18 17:23:31 +0800

    none

---
+++ Projects/ShellProjects/simlpe-game.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 1. for read operation (User)
+# 2. for show context to screen (cat simple-game.data file)
+# 3. for change the context of simple-game.data file (NPC)
+
+# need learn stdin and stdout
